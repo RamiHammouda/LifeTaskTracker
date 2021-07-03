9710ca44206f29e025e574c6cc834e069c91dd33
maher hadj mohamed salah <badpromaher@gmail.com>
2021-07-03 14:39:49 +0200

    Migration fertiger Dateien und Bereinigung

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home.js"
 import Admin from "./components/Admin.js"
@@ -8,11 +9,28 @@ import ViewCertificate from "./components/ViewCertificate.js"
 import ViewAllCertificates from "./components/ViewAllCertificates.js"
 
 import "./App.css";
-import { Switch,Route } from "react-router-dom";
 import UpdateCertificate from "./components/UpdateCertificate";
 import DeleteCertificate from "./components/DeleteCertificate";
 
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
+import UpdateProfile from "./components/Profile/updateProfile";
+import Container from "reactstrap/lib/Container";
+import Row from "reactstrap/lib/Row";
+import Col from "reactstrap/lib/Col";
+import AddCertificate from "./components/AddCertificate.js"
+import ViewCertificate from "./components/ViewCertificate.js"
+import DiplomasList from "components/Profile/DiplomasList";
+import JobsList from "components/Profile/JobsList";
+import ProjectsList from "components/Profile/ProjectsList";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: null };
+  }
   render() {
     if (window.location.host.split(".")[0] === "admin") {
 
@@ -31,6 +49,15 @@ class App extends Component {
 
     );
   }else{
+
+    if (this.state.user == null) {
+      return (<div></div>)
+    }
+
+    if(localStorage.getItem("user")==null){
+      return(<Login />)
+    }
+
     return (
       /* <Switch>
       <Route exact path="/" component={Home}></Route>
@@ -39,13 +66,51 @@ class App extends Component {
       <Route exact component={Error404}/>
       </Switch> */
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route path="/" exact>
+                    <Home user={JSON.parse(localStorage.getItem("user"))} />
+                  </Route>
+                  <Route path="/profile" exact>
+                    <Profile user={JSON.parse(localStorage.getItem("user"))} />
+                  </Route>
+                  <Route path="/profile/update" exact>
+                    <UpdateProfile user={JSON.parse(localStorage.getItem("user"))} />
+                  </Route>
+                  <Route path="/profile/diplomas" exact component={DiplomasList}/>
+                  <Route path="/profile/projects" exact>
+                    <ProjectsList user={JSON.parse(localStorage.getItem("user"))}/>
+                  </Route>
+                  <Route path="/profile/jobs" exact>
+                    <JobsList user={JSON.parse(localStorage.getItem("user"))}/>
+                  </Route>
+                  <Route path="/login">
+                    <Login />
+                  </Route>
+                  <Route path="/sign-up">
+                    <SignUp />
+                  </Route>
+                  <Route exact path="/add" component={AddCertificate}/>
+                  <Route exact path="/view/:hash" component={ViewCertificate}/>
         <Route exact component={Error404}/>
       </Switch>
 
   );
+  getUser() {
+    // console.log("entered here :) hello boi");
+    fetch("http://localhost:5000/users/6035037c49f6b243b4357f7a")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ user: res });
+        localStorage.setItem("user", JSON.stringify(res));
+      })
+      .catch(err => this.setState({ user: err }));
   }
-  }
-}
 
-export default App;
+  // componentDidMount() {
+  //   this.getUser();
+  // }
+
+  x =this.getUser();
+
+
+  }
+
